f2ef8a5ed3f292dacdb8eed0d6b82143e3aaccdb
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2023-05-31 00:03:01 +0530

    api endpoints working

---
+++ src/main/java/com/security/login/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.security.login.auth;
+
+import com.security.login.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
---
+++ src/main/java/com/security/login/user/Role.java
@@ -0,0 +1,9 @@
+package com.security.login.user;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+    USER,
+    ADMIN;
+}
