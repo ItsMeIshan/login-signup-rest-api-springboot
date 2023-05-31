12fe4b0d0ff2654a8071baf7cda47dddb770d864
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2023-05-31 12:20:38 +0530

    fix: fixes 403 Error with valid token for demo-controller

--- src/main/java/com/security/login/config/JwtAuthenticationFilter.java
+++ src/main/java/com/security/login/config/JwtAuthenticationFilter.java
@@ -1,16 +1,12 @@
 package com.security.login.config;
 
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.beans.Transient;
 import java.io.IOException;
 import java.security.Security;
-
-import jakarta.transaction.TransactionScoped;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -34,10 +30,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        if (request.getServletPath().contains("/api/v1/auth")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
@@ -52,7 +44,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
-                        null
+                        null,
+                        userDetails.getAuthorities()
                 );
                 authToken.setDetails(
                         new WebAuthenticationDetailsSource().buildDetails(request)
--- src/main/java/com/security/login/demo/DemoController.java
+++ src/main/java/com/security/login/demo/DemoController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
+
     @GetMapping
     public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("hello");
+        return ResponseEntity.ok("Hello");
     }
+
 }
